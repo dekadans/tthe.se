7c322a3accb995b6443c1ef85a19a840fb74dfcb
Tomas Thelander <tomas.thelander0@gmail.com>
2020-10-20 09:26:47 +0200

    Old website

--- dist/filmfeed.php
+++
@@ -1,7 +0,0 @@
-<?php
-$url = 'https://letterboxd.com/dekadans/rss/';
-$rss = file_get_contents($url);
-
-header('Cache-control: max-age=10000');
-header('Content-type: application/xml');
-echo $rss;
