6db08961e257f0f16d5c0478dfcfef0cc9d1c707
Tomas Thelander <tomas.thelander0@gmail.com>
2026-04-08 20:37:02 +0200

    CV update

--- src/Controllers/MeController.php
+++ src/Controllers/MeController.php
@@ -17,20 +17,28 @@ class MeController
     ) {}
 
     #[Route('/me', name: 'me', methods: ['GET'])]
-    public function __invoke(Request $request): Response
+    public function html(Request $request): Response
     {
-        if (in_array($request->getPreferredFormat(), ['jsonld', 'json'])) {
-            $response = new JsonResponse($this->cv->data, headers: [
-                'Content-Type' => 'application/ld+json',
-            ]);
-        } else {
-            $html = $this->view->render('me/me.html.twig', $this->cv->data);
-            $response = new Response($html);
-        }
+        $html = $this->view->render('me/me.html.twig', $this->cv->data);
+        $response = new Response($html);
 
+        return $this->setCache($request, $response);
+    }
+
+    #[Route('/me.json', name: 'me-json', methods: ['GET'])]
+    public function json(Request $request): Response
+    {
+        $response = new JsonResponse($this->cv->data, headers: [
+            'Content-Type' => 'application/json',
+        ]);
+
+        return $this->setCache($request, $response);
+    }
+
+    private function setCache(Request $request, Response $response): Response
+    {
         $response->setEtag($this->cv->hash, true);
         $response->isNotModified($request);
-
         return $response;
     }
 }
