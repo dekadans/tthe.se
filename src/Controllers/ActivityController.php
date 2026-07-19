eb5d2eb429ad2077898882b775d8915d49f6a896
Tomas Thelander <tomas.thelander0@gmail.com>
2026-07-19 20:04:58 +0200

    Activity widget and redesign drafting

--- src/Controllers/ActivityController.php
+++ src/Controllers/ActivityController.php
@@ -8,16 +8,19 @@ use App\Services\Activity\Transformers\FilmTransformer;
 use App\Services\Activity\Transformers\ListTransformer;
 use App\Services\Activity\Type;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Attribute\Route;
 use tthe\Bagatelle\Http\Attribute\Get;
+use Twig\Environment;
 
 #[Route('/activity')]
 class ActivityController
 {
     public function __construct(
         private ActivityRepository $repository,
-        private ListTransformer $transformer
+        private ListTransformer $transformer,
+        private Environment $twig
     ) {}
 
     #[Get('/{type}', 'activity-json')]
@@ -38,5 +41,23 @@ class ActivityController
         };
 
         return new JsonResponse($this->transformer->transform($list, $resourceTransformer));
+    }
+
+    #[Get('/{type}/widget', 'activity-widget')]
+    public function widget(Type $type): Response
+    {
+        $limit = 3;
+        $list = match ($type) {
+            Type::FILM => $this->repository->films($limit),
+            Type::BOOK => $this->repository->books($limit),
+            default => throw new NotFoundHttpException('This type of activity is not yet supported.')
+        };
+
+        $html = $this->twig->render('activity/widget.html.twig', [
+            'list' => $list,
+            'type' => $type->value,
+        ]);
+
+        return new Response($html);
     }
 }
